8d568b031523d0376f954f24c21f0a5a1432c4cf
crelbee <crlllbee@gmail.com>
2021-03-22 19:36:52 +0300

    8 step

--- src/main/resources/scripts/create_user_inheritance_joined.sql
+++ src/main/resources/scripts/create_user_inheritance_joined.sql
@@ -1,9 +1,15 @@
 
 create table user_inheritance_joined (
+    id serial primary key,
+    name varchar(100)
 );
 
 create table employer_inheritance_joined (
+    id int primary key,
+    company varchar(100)
 );
 
 create table applicant_inheritance_joined (
+    id int primary key,
+    position varchar(100)
 );
