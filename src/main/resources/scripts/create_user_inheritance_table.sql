5da7ce1238e47e32d113d90c6d3426333c495e67
crelbee <crlllbee@gmail.com>
2021-03-22 20:02:05 +0300

    finish

--- src/main/resources/scripts/create_user_inheritance_table.sql
+++ src/main/resources/scripts/create_user_inheritance_table.sql
@@ -1,10 +1,22 @@
 create sequence user_id_seq;
 
 create table user_inheritance_table (
+    id serial primary key,
+    position varchar(100),
+    company varchar(100),
+    name varchar(100)
 );
 
 create table employer_inheritance_table (
+    id serial primary key,
+    position varchar(100),
+    company varchar(100),
+    name varchar(100)
 );
 
 create table applicant_inheritance_table (
+    id serial primary key,
+    position varchar(100),
+    company varchar(100),
+    name varchar(100)
 );
