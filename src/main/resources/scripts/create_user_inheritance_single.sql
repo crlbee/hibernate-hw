c45f4f7ae52a407dbebb17b66ae38315db5f5c2d
crelbee <crlllbee@gmail.com>
2021-03-22 19:27:59 +0300

    7 step

--- src/main/resources/scripts/create_user_inheritance_single.sql
+++ src/main/resources/scripts/create_user_inheritance_single.sql
@@ -1,2 +1,7 @@
 create table user_inheritance_single (
+    id serial primary key,
+    dtype varchar (33) not null,
+    position varchar(100),
+    company varchar(100),
+    name varchar(100)
 );
